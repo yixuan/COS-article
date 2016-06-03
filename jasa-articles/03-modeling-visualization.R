531f53eb8698fb13aaa280bc05fa340d15bd7ce6
Yixuan Qiu <yixuanq@gmail.com>
2016-06-03 21:01:09 +0800

    some cleaning

--- jasa-articles/03-modeling-visualization.R
+++ jasa-articles/03-modeling-visualization.R
@@ -5,27 +5,32 @@ library(ggplot2)
 
 X = cbind(article_dat$page_order, article_dat$issue, sect_mat, title_mat, author_mat)
 Y = log(article_dat$views)
-
-xbar = colMeans(X)
-xss  = colSums(X^2)
-xsd  = sqrt((xss - nrow(X) * xbar^2) / (nrow(X) - 1))
-
-mod = glmnet(X, Y)
-sum(coef(mod, s = 0.0475, exact = TRUE) != 0)
-
 var_names = c("PageOrder [Other]", "IssueNO. [Other]",
               paste(sect_dict, "[Section]"),
               paste(dict, "[Term]"),
               paste(author_dict, "[Author]"))
-cf = coef(mod, s = 0.0475, exact = TRUE)[-1]
-# ind = (abs(cf) > 1e-2)
+
+
+## Used to calculate standardized coefficients
+xbar = colMeans(X)
+xss  = colSums(X^2)
+xsd  = sqrt((xss - nrow(X) * xbar^2) / (nrow(X) - 1))
+
+## Lasso regression
+mod = glmnet(X, Y)
+## Select a penalty parameter that selects 30 variables
+sum(coef(mod, s = 0.048, exact = TRUE) != 0)
+## Exclude intercept
+cf = coef(mod, s = 0.048, exact = TRUE)[-1]
+## Non-zero coefficients
 ind = abs(cf) > 1e-6
+## Coefficients
 vals = cf[ind] * xsd[ind]
+## Variable names
 vars = var_names[ind]
 names(vals) = vars
 
-
-
+## Visualize result
 gd = data.frame(var = vars, coef = vals)
 gd$var = reorder(gd$var, gd$coef)
 gd$sign = factor(-sign(gd$coef))
